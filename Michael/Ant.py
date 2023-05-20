1295f5f388ddd54fca67cb879115e6af62aa48b7
miru8190 <michael_russo@outlook.de>
2023-05-20 21:00:32 +0200

    added sprites

--- Michael/Ant.py
+++ Michael/Ant.py
@@ -6,16 +6,22 @@ import pygame
 
 
 
-class Ant:
+class Ant(pygame.sprite.Sprite):
+
     # Ant-Image WIDTH and HEIGHT Setting
     ANT_IMAGE_WIDTH = 20
     ANT_IMAGE_HEIGHT = 30
-    # Import Ant-Image and resize it
-    ANT_IMAGE = (pygame.transform.scale(pygame.image.load(os.path.join('Assets','ant.png')), (ANT_IMAGE_WIDTH,ANT_IMAGE_HEIGHT))).convert_alpha()
     
 
     def __init__(self, sc, colony ,maxSpeed = 100):
-        self.ant_pos = [sc.WIDTH/2, sc.HEIGHT/2]
+        super(Ant, self).__init__()
+        # Import Ant-Image and resize it
+        self.image = (pygame.transform.scale(pygame.image.load(os.path.join('Assets','ant.png')), (self.ANT_IMAGE_WIDTH,self.ANT_IMAGE_HEIGHT))).convert_alpha()
+        self.rect = self.image.get_rect()
+
+        self.rect.x = sc.WIDTH/2
+        self.rect.y = sc.HEIGHT/2
+
         self.maxSpeed = maxSpeed
         self.colony = colony
         self.angle = 0
@@ -23,13 +29,13 @@ class Ant:
         self.vel_y = 0
 
     def antImage(self):
-        ANT_IMAGE_RECT = self.ANT_IMAGE.get_rect()
+        ANT_IMAGE_RECT = self.rect
           
-        ANT_IMAGE_RECT.center = (self.ant_pos[0], self.ant_pos[1])
+        ANT_IMAGE_RECT.center = (self.rect.x, self.rect.y)
 
         degrees = - (math.degrees(self.angle)-90)
         
-        ROTATED_ANT_IMAGE = pygame.transform.rotate(self.ANT_IMAGE, degrees)
+        ROTATED_ANT_IMAGE = pygame.transform.rotate(self.image, degrees)
         
         ant_img = [ROTATED_ANT_IMAGE, ANT_IMAGE_RECT]
         return ant_img
@@ -44,8 +50,8 @@ class Ant:
     def update(self, elapsed):
         # Update ant position
         self.calc()
-        self.ant_pos[0] += self.vel_x * elapsed
-        self.ant_pos[1] += self.vel_y * elapsed
+        self.rect.x += self.vel_x * elapsed
+        self.rect.y += self.vel_y * elapsed
 
     def setMaxSpeed(newMaxSpeed):
         Ant.MAX_SPEED = newMaxSpeed
--- Michael/Colony.py
+++ Michael/Colony.py
@@ -1,4 +1,5 @@
 from Ant import Ant
+import pygame
 
 class Colony:
     
@@ -6,21 +7,46 @@ class Colony:
     def __init__(self, screen ,ant_count = 1):
         self.ants = []
         self.screen = screen
-        for i in range (ant_count):
-            self.ants.append(Ant(screen,self))
+        self.all_sprites_list = pygame.sprite.Group()
 
+        for ant in range (ant_count):
+            self.ants.append(Ant(screen,self))
         
+        for i in self.ants:
+            self.all_sprites_list.add(i)
 
     def update(self, elapsed):
+        self.all_sprites_list.update(elapsed)
+
         for ant in self.ants:
             ant.update(elapsed)
 
             # Make sure the ant is not moving out of the window
-            if ant.ant_pos[0] <= 0 or ant.ant_pos[0] >= self.screen.WIDTH:
-                ant.ant_pos[0] *= -1
+            if ant.rect.x <= 0 or ant.rect.x >= self.screen.WIDTH:
+                ant.rect.x *= -1
 
-            if ant.ant_pos[1] <= 0 or ant.ant_pos[1] >= self.screen.HEIGHT:
-             ant.ant_pos[1] *= -1
+            if ant.rect.y <= 0 or ant.rect.y >= self.screen.HEIGHT:
+             ant.rect.y *= -1
 
+
+
+
+    # Hier könnten Anweisungen an alle ants der colony erfolgen
+    
+    # Alle Ameisen schwärmen aus und sammeln Essen
+    def collect_food():
+        pass
+
+    # Alle Ameisen attackieren Angreifer
+    def attack():
+        pass
+    
+    # Alle Ameisen verstecken sich im Nest
+    def hide():
+        pass
+
+    # Neue Ameise erzeugen
+    def birth():
+        pass
 
    
